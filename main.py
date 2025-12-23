b4e08f65df43db998d59f95830a8928a0760c812
Manuchet <MC683422050248>
2025-12-23 14:23:46 +0700

    create class EV

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ class car:
 
 my_car = car('toyota','vios')
 my_car.drive()
+
+
+class electriccar:
+    def __init__(self,model,brand,battery_size):
+        super().__init__(model)
+        self.battery_size = battery_size
+
+    def charge(self):
+        print(f'the {self.battery_size}kwh battery is charge')
+
+my_car = car('toyota','vios')
+my_car.drive()
+my_ev = electriccar('tesla','model 3',75)
+my_ev.drive()
+my_ev.charge()
+
+    
+        
