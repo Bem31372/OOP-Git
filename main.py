25567d4fef266c016819e0a1d9a20df4cbd88408
Manuchet <MC683422050248>
2025-12-23 16:42:17 +0700

    Add breaking sytem

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 class car:
-    def __init__(self,model,brand):
+    def __init__(self,model,brand,is_moving=True):
         self.model = model
         self.brand = brand
+        self.is_moving = is_moving
 
     def drive(self):
         print(f'{self.brand},{self.model} is driving')
 
-my_car = car('toyota','vios')
-my_car.drive()
+    def apply_brake(self):
+        print(f"slowing down ={self.is_moving}")
+    
 
-
-class electriccar:
-    def __init__(self,model,brand,battery_size):
-        super().__init__(model)
+class electriccar(car):
+    def __init__(self,model,brand,battery_size,is_moving=True):
+        super().__init__(model,brand)
         self.battery_size = battery_size
+        self.is_moving = is_moving
 
     def charge(self):
-        print(f'the {self.battery_size}kwh battery is charge')
+        print(f'the {self.battery_size} kwh battery is charge')
 
 my_car = car('toyota','vios')
 my_car.drive()
-my_ev = electriccar('tesla','model 3',75)
-my_ev.drive()
-my_ev.charge()
+ev_car= electriccar('tesla','model 3',75)
+ev_car.drive()
+ev_car.charge()
+my_car.apply_brake()
+
+
 
     
         
